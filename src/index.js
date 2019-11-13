cf4648b6d7710f7178a766b380266572f69fff7d
Ashik Liyakathali <ashik21395@gmail.com>
2019-11-13 16:21:55 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import "babel-polyfill";
+import React from "react";
+import ReactDom from "react-dom";
+
+const App = () => {
+  ReactDom.render(
+    <h1>Sample React Application</h1>,
+    document.getElementById("root")
+  );
+};
+App();
